668fcb6a2c1a4855eaeb1b24fbc22524366668a5
Pratik Chakraborty <pratik@talkinglands.com>
2022-11-24 11:06:09 +0530

    before db

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { EmployeesModule } from './employees/employees.module';
+import { EmployeesService } from './employees/employees.service';
+
+
+@Module({
+  imports: [EmployeesModule],
+  providers: [EmployeesService],
+
+})
+export class AppModule {}
